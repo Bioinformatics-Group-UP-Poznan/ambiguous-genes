98392250718e816d79dc8b873e0f5142bb9a18d5
alicjaszabelska <aszabelska@gmail.com>
2023-07-15 21:25:33 +0200

    Removed .RData file from repository

--- codes/differential_analysis_functions.R
+++ codes/differential_analysis_functions.R
@@ -1,50 +1,50 @@
-obliczenia.all.methods <- function(county, n.mapper = 3, design, p.val=0.05, method = "edgeR")
+diffAnaAll <- function(counts, n.maper = 3, exp.design, p.val=0.05, method = "edgeR")
 {
   require("edgeR")
   require("DESeq") 
   require("DESeq2")
   require("limma")
-  if (ncol(county) %% n.mapper != 0) {
+  if (ncol(counts) %% n.maper != 0) {
     stop("Number of columns is not a multiplication of number of mappers.")
   }
-  n <- ncol(county)/n.mapper
-  colnames(county) <- paste0("mapper",rep(1:n.mapper, each=n),"-",rep(1:n,times=n.mapper))
-  isexpr <- rowSums(county > 5) >= n/3
-  county <- county[isexpr,]
-  grupy <- as.factor(rep(design, n.mapper)) 
-  mapery <- as.factor(rep(paste0("mapper",1:n.mapper), each = n))
-  n.groups <- length(unique(grupy))
+  n <- ncol(counts)/n.maper
+  colnames(counts) <- paste0("mapper",rep(1:n.maper, each=n),"-",rep(1:n,times=n.maper))
+  isexpr <- rowSums(counts > 5) >= n/3
+  counts <- counts[isexpr,]
+  groups <- as.factor(rep(exp.design, n.maper)) 
+  mapers <- as.factor(rep(paste0("mapper",1:n.maper), each = n))
+  n.groups <- length(unique(groups))
   
   switch(method,
          edgeR = 
          {      
-           design1 <- model.matrix( ~ grupy + mapery)
-           y <- DGEList(county)
+           design1 <- model.matrix( ~ groups + mapers)
+           y <- DGEList(counts)
            y <- calcNormFactors(y)
            y1 <- estimateDisp(y, design1)
            fit <- glmFit(y1, design1)
            lrt1 <- glmLRT(fit, coef=2:n.groups)
-           b <- match(rownames(lrt1$table),rownames(county))
+           b <- match(rownames(lrt1$table),rownames(counts))
            out <- data.frame(ID=rownames(lrt1$table)[b], stat=lrt1$table$LR[b], padj=lrt1$table$PValue[b])
          },
          limma1 = 
          {
-           design1 <- model.matrix( ~ grupy + mapery)
-           lib.sizes <- calcNormFactors(county, method = "TMM")
-           voom.data = voom(county, design = design1,
-                            lib.size = colSums(county) * lib.sizes)
-           voom.data$genes = rownames(county)
+           design1 <- model.matrix( ~ groups + mapers)
+           lib.sizes <- calcNormFactors(counts, method = "TMM")
+           voom.data = voom(counts, design = design1,
+                            lib.size = colSums(counts) * lib.sizes)
+           voom.data$genes = rownames(counts)
            voom.fitlimma1 = lmFit(voom.data, design = design1)
            voom.fitbayes1 = eBayes(voom.fitlimma1)
            res1 <- topTable(voom.fitbayes1, coef = 2:n.groups, number=nrow(voom.fitbayes1), adjust.method="BH", sort.by="none")
-           b <- match(rownames(res1),rownames(county))
-           if(length(unique(grupy)) > 2) out <- data.frame(ID=rownames(res1)[b], stat=res1$F[b], padj=res1$adj.P.Val[b]) else
+           b <- match(rownames(res1),rownames(counts))
+           if(length(unique(groups)) > 2) out <- data.frame(ID=rownames(res1)[b], stat=res1$F[b], padj=res1$adj.P.Val[b]) else
              out <- data.frame(ID=rownames(res1)[b], stat=res1$B[b], padj=res1$adj.P.Val[b])
          },
          limma2 = 
          {
-           design1 <- model.matrix( ~ grupy + mapery)
-           DESeq.cds1 <- newCountDataSet(countData = round(county), conditions = design1)
+           design1 <- model.matrix( ~ groups + mapers)
+           DESeq.cds1 <- newCountDataSet(countData = round(counts), conditions = design1)
            DESeq.cds1 <- estimateSizeFactors(DESeq.cds1)
            lib.sizes <- sizeFactors(DESeq.cds1)
            DESeq.cds1 <- estimateDispersions(DESeq.cds1, method = "blind", fitType = "local")
@@ -52,22 +52,22 @@ obliczenia.all.methods <- function(county, n.mapper = 3, design, p.val=0.05, met
            voom.fitlimma1 = lmFit(DESeq.vst1, design = design1)
            voom.fitbayes1 = eBayes(voom.fitlimma1)
            res1<-topTable(voom.fitbayes1, coef = 2:n.groups, number=nrow(voom.fitbayes1), adjust.method="BH", sort.by="none")
-           b <- match(rownames(res1),rownames(county))
-           if(length(unique(grupy)) > 2) out <- data.frame(ID=rownames(res1)[b], stat=res1$F[b], padj=res1$adj.P.Val[b]) else
+           b <- match(rownames(res1),rownames(counts))
+           if(length(unique(groups)) > 2) out <- data.frame(ID=rownames(res1)[b], stat=res1$F[b], padj=res1$adj.P.Val[b]) else
              out <- data.frame(ID=rownames(res1)[b], stat=res1$B[b], padj=res1$adj.P.Val[b])
          },
          DESeq = 
          {
-           design1 <- data.frame(grupy = grupy, mapery = mapery)
-           cdsFull = newCountDataSet(round(county), design1)
+           design1 <- data.frame(groups, mapers)
+           cdsFull = newCountDataSet(round(counts), design1)
            cdsFull = estimateSizeFactors( cdsFull )
            cdsFull = estimateDispersions( cdsFull )
-           fit0 = fitNbinomGLMs( cdsFull, count ~  grupy + mapery )
-           fit2 = fitNbinomGLMs( cdsFull, count ~  mapery )
+           fit0 = fitNbinomGLMs( cdsFull, count ~  groups + mapers )
+           fit2 = fitNbinomGLMs( cdsFull, count ~  mapers )
 
            pvalsGLM2 = nbinomGLMTest(fit0, fit2)
            padjGLM2 = p.adjust( pvalsGLM2, method="BH" )
-           b <- match(rownames(fit2),rownames(county))
+           b <- match(rownames(fit2),rownames(counts))
            out <- data.frame(ID = rownames(fit0)[b], stat = fit0$`(Intercept)`[b], padj = padjGLM2[b])
          })
   
@@ -75,39 +75,39 @@ obliczenia.all.methods <- function(county, n.mapper = 3, design, p.val=0.05, met
   res
 }
 
-obliczenia.all.methods1<-function(data, klas, method = "edgeR")
+diffAnaAll1 <- function(data, classes, method = "edgeR")
 {
   library(limma)
   library(edgeR)
   library(DESeq)
   library(DESeq2)
-  isexpr <- rowSums(data > 5) >= length(klas)/3
+  isexpr <- rowSums(data > 5) >= length(classes)/3
   data <- data[isexpr,]
   
   switch(method,
          DESeq = 
          { 
-            colData <- data.frame(condition=as.factor(klas))
+            colData <- data.frame(condition=as.factor(classes))
             rownames(colData) <- colnames(data)
             dds <- DESeqDataSetFromMatrix(countData = round(data), colData = colData, design = ~ condition) 
             dds <- DESeq(dds)
             res <- results(dds)
             ID <- rownames(res)
             res <- as.data.frame(res@listData)
-            porownania <- data.frame(ID = ID, stat = res$stat, padj = res$padj)
+            topTab <- data.frame(ID = ID, stat = res$stat, padj = res$padj)
          },
          edgeR = 
          { 
-            y <- DGEList(data, group = klas)
+            y <- DGEList(data, group = classes)
             y <- calcNormFactors(y)
-            if (length(unique(klas)) > 2)
+            if (length(unique(classes)) > 2)
             {
-              design1 <- model.matrix(~ klas)
+              design1 <- model.matrix(~ classes)
               y1 <- estimateDisp(y, design1)
               fit <- glmFit(y1, design1)
-              lrt1 <- glmLRT(fit, coef=2:length(unique(klas)))
+              lrt1 <- glmLRT(fit, coef=2:length(unique(classes)))
               b <- match(rownames(lrt1$table),rownames(data))
-              porownania <- data.frame(ID=rownames(lrt1$table)[b], stat=lrt1$table$LR[b], padj=lrt1$table$PValue[b])
+              topTab <- data.frame(ID=rownames(lrt1$table)[b], stat=lrt1$table$LR[b], padj=lrt1$table$PValue[b])
             } else {
               y <- estimateCommonDisp(y)
               y <- estimateTagwiseDisp(y)
@@ -116,37 +116,37 @@ obliczenia.all.methods1<-function(data, klas, method = "edgeR")
               padj <- p.adjust(et[,3], method='BH')
               res <- data.frame(et ,padj)
               b <- match(rownames(data), rownames(res))
-              porownania <- data.frame(ID = rownames(res)[b], stat = res$logFC[b], padj = res$padj[b])
+              topTab <- data.frame(ID = rownames(res)[b], stat = res$logFC[b], padj = res$padj[b])
             }
          },
          limma1 = 
          {
             #### limma + voom
             nf1 <- calcNormFactors(data, method = "TMM")
-            voom.data <- voom(data, design = model.matrix(~factor(klas)),
+            voom.data <- voom(data, design = model.matrix(~factor(classes)),
                               lib.size = colSums(data) * nf1)
             voom.data$genes <- rownames(data)
-            voom.fitlimma1 <- lmFit(voom.data, design = model.matrix(~factor(klas)))
+            voom.fitlimma1 <- lmFit(voom.data, design = model.matrix(~factor(classes)))
             voom.fitbayes1 <- eBayes(voom.fitlimma1)
-            res <- topTable(voom.fitbayes1, coef = 2:length(unique(klas)), number=nrow(voom.fitbayes1), adjust.method="BH")
+            res <- topTable(voom.fitbayes1, coef = 2:length(unique(classes)), number=nrow(voom.fitbayes1), adjust.method="BH")
             b <- match(rownames(data), rownames(res))
-            if(length(unique(klas)) > 2) porownania <- data.frame(ID = rownames(res)[b], stat = res$F[b], padj = res$adj.P.Val[b]) else
-            porownania <- data.frame(ID = rownames(res)[b], stat = res$B[b], padj = res$adj.P.Val[b])
+            if(length(unique(classes)) > 2) porownania <- data.frame(ID = rownames(res)[b], stat = res$F[b], padj = res$adj.P.Val[b]) else
+              topTab <- data.frame(ID = rownames(res)[b], stat = res$B[b], padj = res$adj.P.Val[b])
          },
          limma2 = 
          {  
             #### limma + vst
-            DESeq.cds1 <- newCountDataSet(countData = round(data), conditions = factor(klas))
+            DESeq.cds1 <- newCountDataSet(countData = round(data), conditions = factor(classes))
             DESeq.cds1 <- estimateSizeFactors(DESeq.cds1)
             DESeq.cds1 <- estimateDispersions(DESeq.cds1, method = "blind", fitType = "local")
             DESeq.vst1 <- DESeq::getVarianceStabilizedData(DESeq.cds1)
-            DESeq.vst.fitlimma1 <- lmFit(DESeq.vst1, design = model.matrix(~factor(klas)))
+            DESeq.vst.fitlimma1 <- lmFit(DESeq.vst1, design = model.matrix(~factor(classes)))
             DESeq.vst.fitbayes1 <- eBayes(DESeq.vst.fitlimma1)
-            res <- topTable(DESeq.vst.fitbayes1, coef = 2:length(unique(klas)), adjust.method="BH", number=nrow(data))
+            res <- topTable(DESeq.vst.fitbayes1, coef = 2:length(unique(classes)), adjust.method="BH", number=nrow(data))
             b <- match(rownames(data), rownames(res))
-            if(length(unique(klas)) > 2) porownania <- data.frame(ID = rownames(res)[b], stat = res$F[b], padj = res$adj.P.Val[b]) else
-            porownania <- data.frame(ID = rownames(res)[b], stat = res$B[b], padj = res$adj.P.Val[b])
+            if(length(unique(classes)) > 2) porownania <- data.frame(ID = rownames(res)[b], stat = res$F[b], padj = res$adj.P.Val[b]) else
+              topTab <- data.frame(ID = rownames(res)[b], stat = res$B[b], padj = res$adj.P.Val[b])
          })
-  res <- porownania
+  res <- topTab
   res
 }
--- codes/google_difficult_genes_functions.R
+++ codes/google_difficult_genes_functions.R
@@ -1,24 +1,24 @@
-wyszukiwarka <- function(county1, county2, uklad, p.val=0.05, method = "edgeR")
+dg.seeker <- function(counts1, counts2, exp.design, p.val = 0.05, method = "edgeR")
 {
   require("edgeR")
   require("DESeq") 
   require("DESeq2")
-  if (any(dim(county1)!=dim(county2))) {
+  if (any(dim(counts1)!=dim(counts2))) {
     stop("Tables of counts have to have the same sizes.")
   }
-  n <- ncol(county1)
-  county <- cbind(county1, county2)
-  colnames(county) <- c(paste0("mapper1-",1:n), paste0("mapper2-",1:n))
-  isexpr <- rowSums(cpm(county) > 5) >= 2
-  county <- county[isexpr,]
-  grupy <- as.factor(c(uklad, uklad)) 
-  mapery <- as.factor(c(rep("mapper1",n),rep("mapper2",n)))
+  n <- ncol(counts1)
+  counts <- cbind(counts1, counts2)
+  colnames(counts) <- c(paste0("mapper1-",1:n), paste0("mapper2-",1:n))
+  isexpr <- rowSums(cpm(counts) > 5) >= 2
+  counts <- counts[isexpr,]
+  groups <- as.factor(c(exp.design, exp.design)) 
+  mapers <- as.factor(c(rep("mapper1",n),rep("mapper2",n)))
   
   switch(method,
   edgeR = 
   {      
-    design1 <- model.matrix( ~ grupy + mapery)
-    y <- DGEList(county)
+    design1 <- model.matrix( ~ groups + mapers)
+    y <- DGEList(counts)
     y <- calcNormFactors(y)
     y1 <- estimateDisp(y, design1)
     fit <- glmFit(y1, design1)
@@ -26,43 +26,43 @@ wyszukiwarka <- function(county1, county2, uklad, p.val=0.05, method = "edgeR")
     lrt2 <- glmLRT(fit, coef = ncol(design1))
     lib.sizes <- y$samples[,3]
     
-    out <- data.frame(counts = county)
+    out <- data.frame(counts)
     out$adj.pval <- p.adjust(lrt2$table$PValue, method = "BH")
     out$status.group <- ifelse(p.adjust(lrt1$table$PValue, method = "BH")<p.val, "Sig", "nonSig")
     out$status.mappers <- ifelse(out$adj.pval <p.val, "Sig", "nonSig")
   },
   limma = 
   {
-    design1 <- model.matrix( ~ grupy + mapery)
-    lib.sizes <- calcNormFactors(county, method = "TMM")
-    voom.data = voom(county, design = design1,
-                     lib.size = colSums(county) * lib.sizes)
-    voom.data$genes = rownames(county)
+    design1 <- model.matrix( ~ groups + mapers)
+    lib.sizes <- calcNormFactors(counts, method = "TMM")
+    voom.data = voom(counts, design = design1,
+                     lib.size = colSums(counts) * lib.sizes)
+    voom.data$genes = rownames(counts)
     voom.fitlimma1 = lmFit(voom.data, design = design1)
     voom.fitbayes1 = eBayes(voom.fitlimma1)
     res1<-topTable(voom.fitbayes1, coef = 2:(ncol(design1)-1), number=nrow(voom.fitbayes1), adjust.method="BH", sort.by="none")
     res2<-topTable(voom.fitbayes1, coef = ncol(design1), number=nrow(voom.fitbayes1), adjust.method="BH", sort.by="none")
-    out <- data.frame(counts = county)
+    out <- data.frame(counts = counts)
     out$adj.pval <- res2$adj.P.Val
     out$status.group <- ifelse(res1$adj.P.Val < p.val, "Sig", "nonSig")
     out$status.mappers <- ifelse(res2$adj.P.Val < p.val, "Sig", "nonSig")
   },
   DESeq = 
   {
-    design1 <- data.frame(grupy = grupy, mapery = mapery)
-    cdsFull = newCountDataSet(round(county), design1)
+    design1 <- data.frame(groups, mapers)
+    cdsFull = newCountDataSet(round(counts), design1)
     cdsFull = estimateSizeFactors( cdsFull )
     cdsFull = estimateDispersions( cdsFull )
-    fit0 = fitNbinomGLMs( cdsFull, count ~  grupy + mapery )
-    fit1 = fitNbinomGLMs( cdsFull, count ~  grupy )
-    fit2 = fitNbinomGLMs( cdsFull, count ~  mapery )
+    fit0 = fitNbinomGLMs( cdsFull, count ~  groups + mapers )
+    fit1 = fitNbinomGLMs( cdsFull, count ~  groups )
+    fit2 = fitNbinomGLMs( cdsFull, count ~  mapers )
     lib.sizes <- sizeFactors(cdsFull)
     
     pvalsGLM1 = nbinomGLMTest(fit0, fit1)
     padjGLM1 = p.adjust( pvalsGLM1, method="BH" )
     pvalsGLM2 = nbinomGLMTest(fit0, fit2)
     padjGLM2 = p.adjust( pvalsGLM2, method="BH" )
-    out <- data.frame(counts = county)
+    out <- data.frame(counts = counts)
     out$adj.pval <- padjGLM1
     out$status.group <- ifelse(padjGLM2 < p.val, "Sig", "nonSig")
     out$status.mappers <- ifelse(padjGLM1 < p.val, "Sig", "nonSig")
@@ -71,31 +71,32 @@ wyszukiwarka <- function(county1, county2, uklad, p.val=0.05, method = "edgeR")
   res <- list()
   res[["stats"]] <- out
   res[["lib.sizes"]]  <- lib.sizes  
+  res[["no.filtered"]] <- length(isexpr)
   res
 }
 
 
-wyszukiwarka5 <- function(county, n.mapper = 3, design, p.val=0.05, method = "edgeR")
+dg.seeker5 <- function(counts, n.mapper = 3, exp.design, p.val = 0.05, method = "edgeR")
 {
   require("edgeR")
   require("DESeq") 
   require("DESeq2")
-  if (ncol(county) %% n.mapper != 0) {
+  if (ncol(counts) %% n.mapper != 0) {
     stop("Number of columns is not a multiplication of number of mappers.")
   }
-  n <- ncol(county)/n.mapper
-  colnames(county) <- paste0("mapper",rep(1:n.mapper, each=n),"-",rep(1:n,times=n.mapper))
-  isexpr <- rowSums(county > 5) >= n/3
-  county <- county[isexpr,]
-  grupy <- as.factor(rep(design, n.mapper)) 
-  mapery <- as.factor(rep(paste0("mapper",1:n.mapper), each = n))
-  n.groups <- length(unique(grupy))
+  n <- ncol(counts)/n.mapper
+  colnames(counts) <- paste0("mapper", rep(1:n.mapper, each = n), "-", rep(1:n, times = n.mapper))
+  isexpr <- rowSums(counts > 5) >= n/3
+  counts <- counts[isexpr,]
+  groups <- as.factor(rep(exp.design, n.mapper)) 
+  mapers <- as.factor(rep(paste0("mapper", 1:n.mapper), each = n))
+  n.groups <- length(unique(groups))
   
   switch(method,
          edgeR = 
          {      
-           design1 <- model.matrix( ~ grupy + mapery)
-           y <- DGEList(county)
+           design1 <- model.matrix( ~ groups + mapers)
+           y <- DGEList(counts)
            y <- calcNormFactors(y)
            y1 <- estimateDisp(y, design1)
            fit <- glmFit(y1, design1)
@@ -103,9 +104,9 @@ wyszukiwarka5 <- function(county, n.mapper = 3, design, p.val=0.05, method = "ed
            lrt2 <- glmLRT(fit, coef =(n.groups+1):ncol(design1))
            lib.sizes <- y$samples[,3]
            
-           b1 <- match(rownames(lrt1$table),rownames(county))
-           b2 <- match(rownames(lrt2$table),rownames(county))
-           out <- data.frame(counts = county)
+           b1 <- match(rownames(lrt1$table),rownames(counts))
+           b2 <- match(rownames(lrt2$table),rownames(counts))
+           out <- data.frame(counts = counts)
            out$pval.gr <- p.adjust(lrt1$table$PValue[b1], method = "BH")
            out$pval.map <- p.adjust(lrt2$table$PValue[b2], method = "BH")
            out$status.group <- ifelse(out$pval.gr < p.val, "Sig", "nonSig")
@@ -113,18 +114,18 @@ wyszukiwarka5 <- function(county, n.mapper = 3, design, p.val=0.05, method = "ed
          },
          limma1 = 
          {
-           design1 <- model.matrix( ~ grupy + mapery)
-           lib.sizes <- calcNormFactors(county, method = "TMM")
-           voom.data = voom(county, design = design1,
-                            lib.size = colSums(county) * lib.sizes)
-           voom.data$genes = rownames(county)
+           design1 <- model.matrix( ~ groups + mapers)
+           lib.sizes <- calcNormFactors(counts, method = "TMM")
+           voom.data = voom(counts, design = design1,
+                            lib.size = colSums(counts) * lib.sizes)
+           voom.data$genes = rownames(counts)
            voom.fitlimma1 = lmFit(voom.data, design = design1)
            voom.fitbayes1 = eBayes(voom.fitlimma1)
            res1<-topTable(voom.fitbayes1, coef = 2:n.groups, number=nrow(voom.fitbayes1), adjust.method="BH", sort.by="none")
            res2<-topTable(voom.fitbayes1, coef = (n.groups+1):ncol(design1), number=nrow(voom.fitbayes1), adjust.method="BH", sort.by="none")
-           out <- data.frame(counts = county)
-           b1 <- match(rownames(res1),rownames(county))
-           b2 <- match(rownames(res2),rownames(county))
+           out <- data.frame(counts = counts)
+           b1 <- match(rownames(res1),rownames(counts))
+           b2 <- match(rownames(res2),rownames(counts))
            out$pval.gr <- res1$adj.P.Val[b1]
            out$pval.map <- res2$adj.P.Val[b2]
            out$status.group <- ifelse(out$pval.gr < p.val, "Sig", "nonSig")
@@ -132,8 +133,8 @@ wyszukiwarka5 <- function(county, n.mapper = 3, design, p.val=0.05, method = "ed
          },
          limma2 = 
          {
-           design1 <- model.matrix( ~ grupy + mapery)
-           DESeq.cds1 <- newCountDataSet(countData = round(county), conditions = design1)
+           design1 <- model.matrix( ~ groups + mapers)
+           DESeq.cds1 <- newCountDataSet(countData = round(counts), conditions = design1)
            DESeq.cds1 <- estimateSizeFactors(DESeq.cds1)
            lib.sizes <- sizeFactors(DESeq.cds1)
            DESeq.cds1 <- estimateDispersions(DESeq.cds1, method = "blind", fitType = "local")
@@ -142,9 +143,9 @@ wyszukiwarka5 <- function(county, n.mapper = 3, design, p.val=0.05, method = "ed
            voom.fitbayes1 = eBayes(voom.fitlimma1)
            res1<-topTable(voom.fitbayes1, coef = 2:n.groups, number=nrow(voom.fitbayes1), adjust.method="BH", sort.by="none")
            res2<-topTable(voom.fitbayes1, coef = (n.groups+1):ncol(design1), number=nrow(voom.fitbayes1), adjust.method="BH", sort.by="none")
-           out <- data.frame(counts = county)
-           b1 <- match(rownames(res1),rownames(county))
-           b2 <- match(rownames(res2),rownames(county))
+           out <- data.frame(counts = counts)
+           b1 <- match(rownames(res1),rownames(counts))
+           b2 <- match(rownames(res2),rownames(counts))
            out$pval.gr <- res1$adj.P.Val[b1]
            out$pval.map <- res2$adj.P.Val[b2]
            out$status.group <- ifelse(out$pval.gr < p.val, "Sig", "nonSig")
@@ -152,22 +153,22 @@ wyszukiwarka5 <- function(county, n.mapper = 3, design, p.val=0.05, method = "ed
          },
          DESeq = 
          {
-           design1 <- data.frame(grupy = grupy, mapery = mapery)
-           cdsFull = newCountDataSet(round(county), design1)
+           design1 <- data.frame(groups = groups, mapers = mapers)
+           cdsFull = newCountDataSet(round(counts), design1)
            cdsFull = estimateSizeFactors( cdsFull )
            cdsFull = estimateDispersions( cdsFull )
-           fit0 = fitNbinomGLMs( cdsFull, count ~  grupy + mapery )
-           fit1 = fitNbinomGLMs( cdsFull, count ~  grupy )
-           fit2 = fitNbinomGLMs( cdsFull, count ~  mapery )
+           fit0 = fitNbinomGLMs( cdsFull, count ~  groups + mapers )
+           fit1 = fitNbinomGLMs( cdsFull, count ~  groups )
+           fit2 = fitNbinomGLMs( cdsFull, count ~  mapers )
            lib.sizes <- sizeFactors(cdsFull)
            
            pvalsGLM1 = nbinomGLMTest(fit0, fit1)
            padjGLM1 = p.adjust( pvalsGLM1, method="BH" )
            pvalsGLM2 = nbinomGLMTest(fit0, fit2)
            padjGLM2 = p.adjust( pvalsGLM2, method="BH" )
-           out <- data.frame(counts = county)
-           b1 <- match(rownames(fit1),rownames(county))
-           b2 <- match(rownames(fit2),rownames(county))
+           out <- data.frame(counts = counts)
+           b1 <- match(rownames(fit1),rownames(counts))
+           b2 <- match(rownames(fit2),rownames(counts))
            out$pval.gr <- padjGLM2[b2]
            out$pval.map <- padjGLM1[b1]
            out$status.group <- ifelse(padjGLM2[b2] < p.val, "Sig", "nonSig")
@@ -181,25 +182,25 @@ wyszukiwarka5 <- function(county, n.mapper = 3, design, p.val=0.05, method = "ed
 }
 
 
-vis.diff <- function(county1, county2, gene.list)
+vis.diff <- function(counts1, counts2, gene.list)
 {
   require(ggplot2, reshape)
   
   if (length(gene.list) > 16) {
     stop("Maximum number of genes to plot is 16.")
   }
-  if (any(dim(county1)!=dim(county2))) {
+  if (any(dim(counts1)!=dim(counts2))) {
     stop("Tables of counts have to have the same sizes.")
   }
   
-  n <- ncol(county1)
+  n <- ncol(counts1)
   dane <- list()
-  dane[["mapper1"]] <- county1[match(gene.list, rownames(county1)),]
-  dane[["mapper2"]] <- county2[match(gene.list, rownames(county2)),]
+  dane[["mapper1"]] <- counts1[match(gene.list, rownames(counts1)),]
+  dane[["mapper2"]] <- counts2[match(gene.list, rownames(counts2)),]
   colnames(dane[[1]]) <- 1:n
   colnames(dane[[2]]) <- 1:n
   res <- melt(dane)
-  res <- cbind(res, geny = rep(rownames(county)[match(gene.list, rownames(county1))], 2*n))
+  res <- cbind(res, geny = rep(rownames(counts)[match(gene.list, rownames(counts1))], 2*n))
   p <- ggplot(data=res, aes(x=variable, y=value, group=L1, color=L1)) +
     geom_line() + theme_bw() + facet_wrap( ~ geny, scales = "free", ncol=4) + 
     theme(legend.position = "bottom", legend.title=element_blank()) +
